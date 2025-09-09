0a2a4ea4eac70015c9e3cefef25d010cd09a8651
Suhail Mirza <mirza.suhail@gmail.com>
2025-09-09 19:11:17 +0530

    name change error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 import Categorys from '../components/Categorys'
-import FeatureProducts from '../components/products/featureProducts'
+import FeatureProducts from '../components/products/FeatureProducts'
 import Products from '../components/products/Products';
 import Footer from "../components/Footer"
 import { useDispatch, useSelector } from 'react-redux';
